da2fb977a57540665880f1dbe0ba4eb2c71af784
brimst0ne <shadowmoon152@gmail.com>
2023-06-01 19:59:49 +0300

    1st of June

--- vkr/vkr/main.cpp
+++ vkr/vkr/main.cpp
@@ -1,16 +1,34 @@
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
 #include <random>
 #include <fstream>
+#include <unordered_set>
 
 using namespace std;
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
+
+bool hasDuplicate(vector<int> vec1, vector<int> vec2) {
+  unordered_set<int> uniqueNumbers;
+
+  for (int num : vec1) {
+    uniqueNumbers.insert(num);
+  }
+
+  for (int num : vec2) {
+    if (uniqueNumbers.find(num) != uniqueNumbers.end()) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 //-------------------------------------------------------------------
 
@@ -412,15 +430,277 @@ vector<int> findKDomSet2(vvi adjMatrix, int n, int k, int alpha, double loopTime
 
 }
 
+//-------------------------------------------------------------------
+
+vector<int> sortVerticesByDegree(
+    const vector<vector<int>>& adjacencyMatrix) {
+  std::vector<int> vertices;
+  for (int i = 0; i < adjacencyMatrix.size(); ++i) {
+    vertices.push_back(i);
+  }
+
+  sort(vertices.begin(), vertices.end(), [&adjacencyMatrix](int a, int b) {
+    int degreeA = 0;
+    int degreeB = 0;
+
+    for (int i = 0; i < adjacencyMatrix.size(); ++i) {
+      degreeA += adjacencyMatrix[a][i];
+      degreeB += adjacencyMatrix[b][i];
+    }
+
+    return degreeA > degreeB;  // Сортировка по убыванию степени
+  });
+
+  return vertices;
+}
+
+//-------------------------------------------------------------------
+
+vector<int> sortVerticesByKNeighbors(const vvi& adjMatrix, vector<int> domSet, int k) {
+
+  vector<int> vertices;
+  for (int i = 0; i < domSet.size(); ++i) {
+    vertices.push_back(domSet[i]);
+  }
+
+  for (int i = 0; i < vertices.size(); i++) {
+    cout << vertices[i] << " ";
+  }
+  cout << endl;
+
+  sort(vertices.begin(), vertices.end(), [&adjMatrix, &domSet, &k, &vertices](int a, int b) {
+
+    vector<int> neighborsA = getVerticesAtDistanceKOrLess(adjMatrix, a, k);
+    cout << "neighbors" << a << ": ";
+    for (int j = 0; j < neighborsA.size(); j++) {
+      cout << neighborsA[j] << " ";
+    }
+    cout << endl;
+    vector<int> neighborsB = getVerticesAtDistanceKOrLess(adjMatrix, b, k);
+    cout << "neighbors" << b << ": ";
+    for (int j = 0; j < neighborsB.size(); j++) {
+      cout << neighborsB[j] << " ";
+    }
+    cout << endl;
+
+    return neighborsA.size() < neighborsB.size();
+
+  });
+
+  for (int i = 0; i < vertices.size(); i++) {
+    cout << vertices[i] << " ";
+  }
+  cout << endl;
+
+  return vertices;
+
+}
+
+//-------------------------------------------------------------------
+
+vector<int> HEU1(vvi adjMatrix, int n, int k) {
+
+  vector<int> domSet;
+  vector<int> sortedVertices = sortVerticesByDegree(adjMatrix);
+
+  for (int i = 0; i < sortedVertices.size(); i++) {
+    cout << sortedVertices[i] << " ";
+  }
+  cout << endl;
+
+  vector<bool> isCovered(n, false);
+
+  for (int i = 0; i < sortedVertices.size(); i++) {
+    if (!isCovered[sortedVertices[i]]) {
+      domSet.push_back(sortedVertices[i]);
+      isCovered[sortedVertices[i]] = true;
+      vector<int> currentNeighbors =
+          getVerticesAtDistanceKOrLess(adjMatrix, sortedVertices[i], k);
+      for (int j = 0; j < currentNeighbors.size(); j++) {
+        isCovered[currentNeighbors[j]] = true;
+      }
+    }
+  }
+
+  cout << k << "-dominating set by heuristic 1: ";
+  for (int i = 0; i < domSet.size(); i++) {
+    if (i > 0) cout << ", ";
+    cout << domSet[i];
+  }
+  cout << endl;
+
+  return domSet;
+
+}
+
+//-------------------------------------------------------------------
+
+vector<int> HEU2(vvi adjMatrix, int n, int k, int alpha, double loopTime) {
+   
+  vector<int> domSet;
+  vector<int> sortedVertices = sortVerticesByDegree(adjMatrix);
+
+  for (int i = 0; i < sortedVertices.size(); i++) {
+    cout << sortedVertices[i] << " ";
+  }
+  cout << endl;
+
+  vector<bool> isCovered(n, false);
+  double workTime = 0;
+
+  for (int i = 0; i < sortedVertices.size(); i++) {
+
+    double start = clock() / 1000.0;
+
+    if (workTime >= loopTime ||
+        find(isCovered.begin(), isCovered.end(), false) == isCovered.end()) {
+      break;
+    }
+
+    vector<int> kNeighbors = getVerticesAtDistanceKOrLess(adjMatrix, sortedVertices[i], k);
+    vector<int> notDominatedNeighbors;
+
+    for (int k = 0; k < kNeighbors.size(); k++) {
+      if (!isCovered[kNeighbors[k]]) {
+        notDominatedNeighbors.push_back(kNeighbors[k]);
+      }
+    }
+
+    if (!isCovered[sortedVertices[i]] || notDominatedNeighbors.size() >= alpha) {
+      domSet.push_back(sortedVertices[i]);
+      isCovered[sortedVertices[i]] = true;
+      for (int j = 0; j < kNeighbors.size(); j++) {
+        isCovered[kNeighbors[j]] = true;
+      }
+    }
+    double finish = clock() / 1000.0;
+    workTime = finish - start;
+    cout << workTime << endl;
+  }
+
+  for (int i = 0; i < sortedVertices.size(); i++) {
+    if (!isCovered[sortedVertices[i]]) {
+      cout << "HEU1 WORKING" << endl;
+      domSet.push_back(sortedVertices[i]);
+      isCovered[sortedVertices[i]] = true;
+      vector<int> currentNeighbors =
+          getVerticesAtDistanceKOrLess(adjMatrix, sortedVertices[i], k);
+      for (int j = 0; j < currentNeighbors.size(); j++) {
+        isCovered[currentNeighbors[j]] = true;
+      }
+    }
+  }
+
+  cout << k << "-dominating set by heuristic 2: ";
+  for (int i = 0; i < domSet.size(); i++) {
+    if (i > 0) cout << ", ";
+    cout << domSet[i];
+  }
+  cout << endl;
+
+  return domSet;
+
+}
+
+//-------------------------------------------------------------------
+
+vector<int> redundantRemoval(vvi adjMatrix, vector<int> domSet, int k) {
+
+  vector<int> reducedDomSet = domSet;
+
+  vector<int> sortedVertices = sortVerticesByKNeighbors(adjMatrix, domSet, k);
+  for (int i = 0; i < sortedVertices.size(); i++) {
+    cout << sortedVertices[i] << " ";
+  }
+  cout << endl;
+  
+  int k1 = floor((k + 1) / 2);
+  int k2 = k - k1;
+
+  for (int i = 0; i < sortedVertices.size(); i++) {
+    bool S = true;
+    vector<int> kNeighbors =
+        getVerticesAtDistanceKOrLess(adjMatrix, sortedVertices[i], k);
+    for (int j = 0; j < kNeighbors.size(); j++) {
+      bool T = false;
+      vector<int> uKNeighbors =
+          getVerticesAtDistanceKOrLess(adjMatrix, kNeighbors[j], k1);
+      for (int k = 0; k < sortedVertices.size(); k++) {
+        if (sortedVertices[k] != sortedVertices[i]) {
+          vector<int> wKNeighbors =
+              getVerticesAtDistanceKOrLess(adjMatrix, sortedVertices[k], k2);
+          if (hasDuplicate(uKNeighbors, wKNeighbors)) {
+            T = true;
+            break;
+          }
+        }
+      }
+      if (!T) {
+        S = false;
+        break;
+      }
+    }
+    if (S) {
+      remove(reducedDomSet.begin(), reducedDomSet.end(), sortedVertices[i]);
+    }
+  }
+
+  cout << "Reduced Dom Set: " << endl;
+  for (int i = 0; i < reducedDomSet.size(); i++) {
+    cout << reducedDomSet[i] << " ";
+  }
+  cout << endl;
+
+  return reducedDomSet;
+
+}
+
+//-------------------------------------------------------------------
+
+vector<vector<int>> BA_graph(int n, int m) {
+  vector<vector<int>> graph(n, vector<int>(n));
+  for (int i = 0; i < m; i++) {
+    for (int j = i + 1; j < m; j++) {
+      graph[i][j] = 1;
+      graph[j][i] = 1;
+    }
+  }
+  for (int i = m; i < n; i++) {
+    int degree_sum = 0;
+    vector<int> degree_list(i, 0);
+    for (int j = 0; j < i; j++) {
+      for (int k = 0; k < i; k++) {
+        degree_sum += graph[j][k];
+        degree_list[k] = degree_sum;
+      }
+      int rand_degree = rand() % degree_sum;
+      int target_node = i;
+      for (int k = 0; k < i; k++) {
+        if (rand_degree < degree_list[k]) {
+          target_node = k;
+          break;
+        }
+      }
+      graph[i][target_node] = 1;
+      graph[target_node][i] = 1;
+    }
+  }
+  return graph;
+}
+
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+
 
 int main() {
   srand(time(NULL));  // используем текущее время в качестве зерна для
                       // генератора случайных чисел
   //int n = rand() % 10 + 2;  // генерируем число вершин в диапазоне от 1 до 500
   //int m = rand() % (n * (n - 1) / 2) + 1;  // генерируем число ребер в диапазоне от 1 до n * (n - 1) / 2
-  int n = 7 + rand() % (10 - 7 + 1);
+  int n = 20 + rand() % (40 - 20 + 1);
   int m = rand() % (n * (n - 1) / 2) + 1;
   vvi adjMatrix = generateGraph(n, m);
+  //vvi adjMatrix = BA_graph(n, 2);
   // выводим граф в виде матрицы смежности
   cout << "Generated matrix: " << endl;
   cout << endl;
@@ -486,11 +766,23 @@ int main() {
   string graphName = "exampleGraph";
   //string treeName = "exampleTree";
 
-  vector<int> kDomSet = findKDomSet1(newAdjMatrix, nn, 2);
+  //vector<int> kDomSet = findKDomSet1(newAdjMatrix, nn, 2);
+  unsigned int start = clock();
+  vector<int> kDomSet1 = HEU1(newAdjMatrix, nn, 1);
+  unsigned int time = clock() - start;
+  cout << "HEU1 work time = " << time / 1000.0 << endl;
 
+  start = clock();
+  vector<int> kDomSet2 = HEU2(newAdjMatrix, nn, 1, 2, 0.001);
+  time = clock() - start;
+  cout << "HEU2 work time = " << time / 1000.0 << endl;
+
+  vector<int> reducedDomSet = redundantRemoval(newAdjMatrix, kDomSet2, 1);
   //vector<int> kDomSet = findMinDomSet(newAdjMatrix, nn, 2);
 
-  generateGraphvizFile(newAdjMatrix, graphName, kDomSet);
+  generateGraphvizFile(newAdjMatrix, "HEU1", kDomSet1);
+  generateGraphvizFile(newAdjMatrix, "HEU2", kDomSet2);
+  generateGraphvizFile(newAdjMatrix, "ReducedDomSet", reducedDomSet);
   //generateGraphvizFile(treeAdjMatrix, treeName);
 
   
